10aab621758fed0c74281c92553c80941895d4b3
Reloxed <antoniomma97@gmail.com>
2020-07-19 16:24:49 +0200

    Eliminamos errores de carga

--- mmsport/lib/screens/choose_sport_school.dart
+++ mmsport/lib/screens/choose_sport_school.dart
@@ -27,36 +27,47 @@ class _ChooseSportSchoolState extends State<ChooseSportSchool> {
   PageController _pageController = PageController();
 
   // ignore: missing_return
-  Future<QuerySnapshot> _loadFirebaseData() async {
+  Future<Map<SportSchool, int>> _loadFirebaseData() async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     String firebaseUser = preferences.get("loggedInUserId");
+    Map<SportSchool, int> auxMap = new Map<SportSchool, int>();
     await Firestore.instance
         .collection("socialProfiles")
         .where('userAccountId', isEqualTo: firebaseUser)
         .getDocuments()
-        .then((value) => value.documents.forEach((element) {
-              SocialProfile newProfile = SocialProfile.socialProfileFromMap(element.data);
-              newProfile.id = element.documentID;
-              profiles.add(newProfile);
-            }));
-    for (SocialProfile actualProfile in profiles) {
-      await Firestore.instance.collection('sportSchools').document(actualProfile.sportSchoolId).get().then((document) {
-        SportSchool newSportSchool = SportSchool.sportSchoolFromMap(document.data);
-        newSportSchool.id = actualProfile.sportSchoolId;
-        if (sportSchoolIds.contains(newSportSchool.id)) {
-          sportSchools[newSportSchool] += 1;
-        }
-        else if(!sportSchoolIds.contains(newSportSchool.id)){
-          sportSchools[newSportSchool] = 1;
-          sportSchoolIds.add(newSportSchool.id);
-        }
+        .then((value) {
+      value.documents.forEach((element) async {
+        SocialProfile newProfile = SocialProfile.socialProfileFromMap(element.data);
+        newProfile.id = element.documentID;
+        profiles.add(newProfile);
       });
-    }
+    });
+      for (SocialProfile actualProfile in profiles) {
+        await Firestore.instance.collection('sportSchools').document(actualProfile.sportSchoolId).get().then((document) {
+          SportSchool newSportSchool = SportSchool.sportSchoolFromMap(document.data);
+          newSportSchool.id = actualProfile.sportSchoolId;
+          if (sportSchoolIds.contains(newSportSchool.id)) {
+            for (int i = 0; i <= auxMap.keys.length - 1; i++) {
+              if (auxMap.keys
+                  .elementAt(i)
+                  .id == newSportSchool.id) {
+                SportSchool aux = auxMap.keys.elementAt(i);
+                auxMap[aux] += 1;
+              }
+            }
+          } else {
+            auxMap[newSportSchool] = 1;
+            sportSchoolIds.add(newSportSchool.id);
+          }
+        });
+      }
+    sportSchools = auxMap;
+    return sportSchools;
   }
 
   @override
   void initState() {
-    _pageController.addListener((){
+    _pageController.addListener(() {
       setState(() {
         currentPage = _pageController.page;
       });
@@ -66,56 +77,60 @@ class _ChooseSportSchoolState extends State<ChooseSportSchool> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
+    return FutureBuilder<Map<SportSchool, int>>(
       future: _loadFirebaseData(),
       builder: (context, profilesSnapshot) {
-        if (!profilesSnapshot.hasData) {}
-        return Material(
-            child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Mis escuelas'),
-            centerTitle: true,
-            actions: <Widget>[
-              _logoutButton(),
-            ],
-          ),
-          body: Center(
-              child: SingleChildScrollView(
-            padding: EdgeInsets.all(20.0),
-            child: Center(
-                child: SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.75,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Container(height: MediaQuery.of(context).size.height * 0.5, child: _pageView()),
-                        Align(
-                          alignment: Alignment.center,
-                          child: Container(
-                            margin: EdgeInsets.all(16.0),
-                            child: _smoothPageIndicator(),
-                          ),
-                        )
-
-                      ],
-                    ))),
-          )),
-          floatingActionButton: FloatingActionButton(
-            onPressed: null,
-            tooltip: 'Inscribirme en una escuela',
-            child: IconButton(
-              onPressed: () {
-                navigateToInscriptionListSportSchool(context);
-              },
-              icon: new IconTheme(
-                  data: new IconThemeData(
-                    color: Colors.white,
-                  ),
-                  child: Icon(Icons.add)),
+        if (profilesSnapshot.hasData) {
+          return Material(
+              child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Mis escuelas'),
+              centerTitle: true,
+              actions: <Widget>[
+                _logoutButton(),
+              ],
             ),
-          ),
-        ));
+            body: Center(
+                child: SingleChildScrollView(
+              padding: EdgeInsets.all(20.0),
+              child: Center(
+                  child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.75,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                              height: MediaQuery.of(context).size.height * 0.5,
+                              child: _pageView(context, profilesSnapshot.data)),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Container(
+                              margin: EdgeInsets.all(16.0),
+                              child: _smoothPageIndicator(),
+                            ),
+                          )
+                        ],
+                      ))),
+            )),
+            floatingActionButton: FloatingActionButton(
+              onPressed: null,
+              tooltip: 'Inscribirme en una escuela',
+              child: IconButton(
+                onPressed: () {
+                  navigateToInscriptionListSportSchool(context);
+                },
+                icon: new IconTheme(
+                    data: new IconThemeData(
+                      color: Colors.white,
+                    ),
+                    child: Icon(Icons.add)),
+              ),
+            ),
+          ));
+        } else {
+          return CircularProgressIndicator();
+        }
       },
     );
   }
@@ -133,40 +148,37 @@ class _ChooseSportSchoolState extends State<ChooseSportSchool> {
     );
   }
 
-  void _logout() async{
+  void _logout() async {
     await FirebaseAuth.instance.signOut();
     logout(context);
   }
 
-  Widget _pageView() {
+  Widget _pageView(BuildContext context, Map<SportSchool, int> profilesSnapshot) {
     return PageView.builder(
-      itemCount: sportSchools.length,
+      itemCount: profilesSnapshot.keys.length,
       controller: _pageController,
-      itemBuilder: (BuildContext context, int index) {
-        return _cardView(index);
+      itemBuilder: (context, int index) {
+        return _cardView(profilesSnapshot.keys.elementAt(index), profilesSnapshot.values.elementAt(index));
       },
     );
   }
 
-  Widget _smoothPageIndicator(){
+  Widget _smoothPageIndicator() {
     return SmoothPageIndicator(
-      controller: _pageController,  // PageController
-      count:  sportSchools.keys.length,
-      effect:  WormEffect(
-          dotColor:  Colors.grey,
-          activeDotColor:  Colors.blueAccent
-      ),  // your preferred effect
+      controller: _pageController, // PageController
+      count: sportSchools.keys.length,
+      effect: WormEffect(dotColor: Colors.grey, activeDotColor: Colors.blueAccent), // your preferred effect
     );
   }
 
-  Widget _cardView(int index) {
+  Widget _cardView(SportSchool sportSchool, int index) {
     return Card(
         elevation: 2.0,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15.0))),
         child: InkWell(
           onTap: () async {
             SharedPreferences preferences = await SharedPreferences.getInstance();
-            String sportSchoolToJson = jsonEncode(sportSchools.keys.elementAt(index).sportSchoolToJson());
+            String sportSchoolToJson = jsonEncode(sportSchool.sportSchoolToJson());
             preferences.setString("chosenSportSchool", sportSchoolToJson);
             navigateToChooseSocialProfile(context);
           },
@@ -180,7 +192,7 @@ class _ChooseSportSchoolState extends State<ChooseSportSchool> {
                         radius: 65,
                         child: ClipOval(
                           child: Image.network(
-                            sportSchools.keys.elementAt(index).urlLogo,
+                            sportSchool.urlLogo,
                             fit: BoxFit.cover,
                             width: 120,
                             height: 120,
@@ -190,47 +202,39 @@ class _ChooseSportSchoolState extends State<ChooseSportSchool> {
               Container(
                   margin: EdgeInsets.fromLTRB(4.0, 8.0, 4.0, 0.0),
                   child: Text(
-                    sportSchools.keys.elementAt(index).name,
+                    sportSchool.name,
                     style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
                   )),
               Container(
                   margin: EdgeInsets.fromLTRB(2.0, 8.0, 4.0, 0.0),
                   child: Text(
-                    sportSchools.values.elementAt(index).toString() + ' Perfiles sociales verificados',
+                    index.toString() + ' Perfiles sociales verificados',
                     style: TextStyle(fontSize: 14.0),
                   )),
               Expanded(
-                child: Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                        margin: EdgeInsets.all(4.0),
-                        child: OutlineButton.icon(
-                          onPressed: (){
-                            navigateToChooseSocialProfile(context);
-                          },
-                          icon: new IconTheme(
-                              data: new IconThemeData(
-                                color: Colors.blueAccent,
-                              ),
-                              child: Icon(Icons.add)),
-                          label: Text(
-                            'Añadir Perfil Social',
-                            style: TextStyle(fontSize: 20.0),
-                          ),
-                          borderSide: BorderSide(
-                              color: Colors.blueAccent,
-                              style: BorderStyle.solid,
-                              width: 2.0
-                          ),
-                          textColor: Colors.blueAccent,
-                          highlightElevation: 3.0,
-                        )))
-              )
-
+                  child: Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                          margin: EdgeInsets.all(4.0),
+                          child: OutlineButton.icon(
+                            onPressed: () {
+                              navigateToChooseSocialProfile(context);
+                            },
+                            icon: new IconTheme(
+                                data: new IconThemeData(
+                                  color: Colors.blueAccent,
+                                ),
+                                child: Icon(Icons.add)),
+                            label: Text(
+                              'Añadir Perfil Social',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            borderSide: BorderSide(color: Colors.blueAccent, style: BorderStyle.solid, width: 2.0),
+                            textColor: Colors.blueAccent,
+                            highlightElevation: 3.0,
+                          ))))
             ],
           ),
         ));
-
-
   }
 }
