ae033d130ad2041fb54ca46f0474c09ac66bc91b
Reloxed <antoniomma97@gmail.com>
2020-07-22 18:50:02 +0200

    Overflow en los textos arreglado

--- mmsport/lib/screens/chats/chat_room.dart
+++ mmsport/lib/screens/chats/chat_room.dart
@@ -31,13 +31,13 @@ class _ChatRoom extends State<ChatRoom> {
     if (loggedSocialProfile.id == chosenChatRoom.users[0]) {
       DocumentSnapshot aux =
           await Firestore.instance.collection("socialProfiles").document(chosenChatRoom.users[1]).get();
-        socialProfileToChat = SocialProfile.socialProfileFromMap(aux.data);
-        socialProfileToChat.id = aux.data['id'];
+      socialProfileToChat = SocialProfile.socialProfileFromMap(aux.data);
+      socialProfileToChat.id = aux.data['id'];
     } else {
       DocumentSnapshot aux =
           await Firestore.instance.collection("socialProfiles").document(chosenChatRoom.users[0]).get();
-        socialProfileToChat = SocialProfile.socialProfileFromMap(aux.data);
-        socialProfileToChat.id = aux.data['id'];
+      socialProfileToChat = SocialProfile.socialProfileFromMap(aux.data);
+      socialProfileToChat.id = aux.data['id'];
     }
     return socialProfileToChat;
   }
@@ -54,40 +54,46 @@ class _ChatRoom extends State<ChatRoom> {
     return FutureBuilder<List<dynamic>>(
         future: Future.wait([_loadDataChatRoom(), _loadDataSocialProfile()]),
         builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-          if(snapshot.hasData){
-          return Material(
-              child: Scaffold(
-                  appBar: PreferredSize(
-                    preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
-                    child: AppBar(
-                      leading: IconButton(
-                        icon: Icon(Icons.arrow_back),
-                        onPressed: () async {
-                          SharedPreferences preferences = await SharedPreferences.getInstance();
-                          preferences.remove("chosenChatRoom");
-                          Navigator.pop(context);
-                        },
-                      ),
-                      title: FlexibleSpaceBar(
-                        centerTitle: false,
-                        titlePadding: EdgeInsets.all(5),
-                        title: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            CircleAvatar(
-                              radius: 30,
-                              backgroundImage: NetworkImage(snapshot.data[1].urlImage),
-                            ),
-                            Container(
-                                padding: EdgeInsets.all(10),
-                                child: Text(snapshot.data[1].name + " " + snapshot.data[1].firstSurname))
-                          ],
+          if (snapshot.hasData) {
+            return Material(
+                child: Scaffold(
+                    appBar: PreferredSize(
+                      preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.1),
+                      child: AppBar(
+                        leading: IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () async {
+                            SharedPreferences preferences = await SharedPreferences.getInstance();
+                            preferences.remove("chosenChatRoom");
+                            Navigator.pop(context);
+                          },
+                        ),
+                        title: FlexibleSpaceBar(
+                          centerTitle: false,
+                          titlePadding: EdgeInsets.all(5),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundImage: NetworkImage(snapshot.data[1].urlImage),
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width * 0.55,
+                                  child: Container(
+                                      padding: EdgeInsets.all(10),
+                                      child: Text(
+                                        snapshot.data[1].name + " " + snapshot.data[1].firstSurname,
+                                        overflow: TextOverflow.ellipsis,
+                                        softWrap: true
+                                      )))
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  body: Text(snapshot.data[0].id)));
-          }else{
+                    body: Text(snapshot.data[0].id)));
+          } else {
             return CircularProgressIndicator();
           }
         });
--- mmsport/lib/screens/homes/home.dart
+++ mmsport/lib/screens/homes/home.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mmsport/constants/constants.dart';
 import 'package:mmsport/models/socialProfile.dart';
 import 'package:mmsport/models/sportSchool.dart';
+import 'package:mmsport/navigations/navigations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'menu_helper.dart';
 import 'package:mmsport/screens/homes/menu_helper.dart';
-
 
 class Home extends StatefulWidget {
   @override
@@ -43,66 +46,85 @@ class _Home extends State<Home> {
     return FutureBuilder<List<dynamic>>(
         future: Future.wait([_loadSportSchool(), _loadSocialProfile()]),
         builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshots) {
-          if(snapshots.hasData){
-          return Scaffold(
-            appBar: PreferredSize(
-              preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.15),
-              child: AppBar(
-                flexibleSpace: FlexibleSpaceBar(
-                    titlePadding: EdgeInsets.all(10.0),
-                    centerTitle: true,
-                    title: Center(
-                        child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[
-                          Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-                            CircleAvatar(radius: 40, backgroundImage: NetworkImage(snapshots.data[0].urlLogo)),
-                            Container(
-                                padding: EdgeInsets.all(10.0),
-                                child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(snapshots.data[0].name, style: TextStyle(color: Colors.white, fontSize: 30)),
-                                      Text(
-                                          snapshots.data[1].name +
-                                              " " +
-                                              snapshots.data[1].firstSurname +
-                                              " " +
-                                              snapshots.data[1].secondSurname,
-                                          style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic))
-                                    ])),
-                          ]),
-                          PopupMenuButton<int>(
-                              itemBuilder: (context) => [
-                                PopupMenuItem(
-                                    value: 1,
-                                    child: Row(children: <Widget>[Icon(Icons.accessibility), Text(" Cambiar perfil")])),
-                                PopupMenuItem(
-                                    value: 2,
-                                    child: Row(children: <Widget>[Icon(Icons.school), Text(" Cambiar escuela")])),
-                                PopupMenuItem(
-                                    value: 3,
-                                    child:
-                                    Row(children: <Widget>[Icon(Icons.power_settings_new), Text(" Cerrar sesión")]))
-                              ],
-                              icon: Icon(Icons.more_vert, color: Colors.white, size: 35.0,),
-                              onSelected: (value) {
-                                if (value == 1) {
-                                  // TODO: Cambiar escuela
-                                } else if (value == 2) {
-                                  // TODO: Cambiar perfil
-                                } else if (value == 3) {
-                                  // TODO: Cerrar sesión
-                                }
-                              })
-                        ]))),
+          if (snapshots.hasData) {
+            return Scaffold(
+              appBar: PreferredSize(
+                preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 0.15),
+                child: AppBar(
+                  flexibleSpace: FlexibleSpaceBar(
+                      titlePadding: EdgeInsets.all(10.0),
+                      centerTitle: true,
+                      title: Center(
+                          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <Widget>[
+                        Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+                          CircleAvatar(radius: 40, backgroundImage: NetworkImage(snapshots.data[0].urlLogo)),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.58,
+                              child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          snapshots.data[0].name,
+                                          style: TextStyle(color: Colors.white, fontSize: 30),
+                                          overflow: TextOverflow.ellipsis,
+                                          softWrap: true,
+                                        ),
+                                        Text(
+                                            snapshots.data[1].name +
+                                                " " +
+                                                snapshots.data[1].firstSurname +
+                                                " " +
+                                                snapshots.data[1].secondSurname,
+                                            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
+                                            overflow: TextOverflow.ellipsis,
+                                            softWrap: true)
+                                      ]))),
+                        ]),
+                        PopupMenuButton<int>(
+                            itemBuilder: (context) => [
+                                  PopupMenuItem(
+                                      value: 1,
+                                      child:
+                                          Row(children: <Widget>[Icon(Icons.accessibility), Text(" Cambiar perfil")])),
+                                  PopupMenuItem(
+                                      value: 2,
+                                      child: Row(children: <Widget>[Icon(Icons.school), Text(" Cambiar escuela")])),
+                                  PopupMenuItem(
+                                      value: 3,
+                                      child: Row(
+                                          children: <Widget>[Icon(Icons.power_settings_new), Text(" Cerrar sesión")]))
+                                ],
+                            icon: Icon(
+                              Icons.more_vert,
+                              color: Colors.white,
+                              size: 35.0,
+                            ),
+                            onSelected: (value) {
+                              if (value == 1) {
+                                // TODO: Cambiar escuela
+                              } else if (value == 2) {
+                                // TODO: Cambiar perfil
+                              } else if (value == 3) {
+                                _logout();
+                              }
+                            })
+                      ]))),
+                ),
               ),
-            ),
-            body: menuGrid(context, snapshots.data[1].role),
-          );} else {
+              body: menuGrid(context, snapshots.data[1].role),
+            );
+          } else {
             return CircularProgressIndicator();
           }
         });
   }
 
-
+  void _logout() async {
+    deleteLoggedInUserId();
+    await FirebaseAuth.instance.signOut();
+    logout(context);
+  }
 }
